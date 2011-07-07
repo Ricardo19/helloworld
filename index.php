59dc321df8296b4f2c590a71e5847a324594571b
Ricardo19 <ricardofilipe19@gmail.com>
2011-07-07 02:08:35 +0100

    Signed-off-by: Ricardo19 <ricardofilipe19@gmail.com>

--- index.php
+++ index.php
@@ -1,17 +1,56 @@
-<!--
-To change this template, choose Tools | Templates
-and open the template in the editor.
--->
+<?php session_start(); ?>
+<!--?php include 'functions.php'; ?-->
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
-    </head>
-    <body>
-        teste
-        <?php
-        // put testyour code here
-        ?>
-    </body>
+   <head>
+      <title>helloworld</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <?php include "layouts/stylesheets.php" ?>
+   </head>
+   <body>
+      <div class="container">
+         <?php include "layouts/header.php" ?>
+         <section class="round">
+            <?php include "layouts/flash_messages.php" ?>
+
+            <?php
+
+            //SE O UTILIZADOR ESTÁ COM LOGIN FEITO
+            if (isset($_SESSION['user'])) {
+               $username = $_SESSION['user'];
+               ?>
+               <h4>Bem-vindo <b><?php echo $username; ?></b></h4>
+               <?php
+
+               if (isset($_SESSION['admin'])) {
+                  ?>
+                  <a href="conta_Pessoal_Admin.php" class="signup_button round">Perfil pessoal</a>
+                  <?php
+
+               }
+               else {
+                  ?>
+                  <a href="conta_Pessoal_User.php" class="signup_button round">Perfil pessoal</a>
+
+                  <?php
+
+               }
+            }
+            else {
+               ?>
+<?php } ?>
+         </section>
+<?php include "layouts/footer.php" ?>
+          
+           <table style="border: 1px solid #000000;">
+           <tr>
+               <td>
+                   
+               </td>
+           </tr>
+           
+       </table>
+      </div>
+   </body>
 </html>
